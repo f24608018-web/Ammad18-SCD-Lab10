2f0635d1d08485fec3bd8cdc8cb68a7b5f3ae5c7
maddy <meinbanjara7@gmail.com>
2026-05-17 22:10:22 +0500

    Main branch modifies login method

--- UserAuthentication.java
+++ UserAuthentication.java
@@ -1,5 +1,6 @@
 public class UserAuthentication {
     public void login(String username, String password) {
         System.out.println("Logging in: " + username);
+        System.out.println("Main version: " + username);
     }
 }
