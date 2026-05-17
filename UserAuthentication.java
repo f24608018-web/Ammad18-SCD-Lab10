07ec34e30a9bf422c1aa56a360d42d80e8e376cd
maddy <meinbanjara7@gmail.com>
2026-05-17 22:32:56 +0500

    Revert "Main branch modifies login method"
    This reverts commit 2f0635d1d08485fec3bd8cdc8cb68a7b5f3ae5c7.

--- UserAuthentication.java
+++ UserAuthentication.java
@@ -1,6 +1,5 @@
 public class UserAuthentication {
     public void login(String username, String password) {
         System.out.println("Logging in: " + username);
-        System.out.println("Main version: " + username);
     }
 }
